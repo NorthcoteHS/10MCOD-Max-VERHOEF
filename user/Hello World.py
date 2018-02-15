8a8079fb2452a1351e759200b1a0e91107d0542c
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-15 12:31:32 +1100

    VERHOEF, Max: 2018-02-15 12:31:32

---
+++ user/Hello World.py
@@ -0,0 +1,2 @@
+h = input("Enter Your Name ") #this is so people can type in their name
+print('Hello, ' + h + " my name is Max") #This prints the text
---
+++ user/calculator.py
@@ -0,0 +1,18 @@
+d = int(input("How many numbers do you want to use?")
+e = 1
+a = float(input("Enter a number "))
+if d >         
+b = float(input("Enter another number "))
+print("what do you want to do with these numbers?")
+c = int(input("add = 1, subtract = 2, multiply = 3, divide = 4, To the power of = 5 "))
+if c == 1:
+    print(a + b)
+if c == 2:
+    print(a - b)
+if c == 3:
+    print(a * b)
+if c == 4:
+    print(a / b)
+if c == 5:
+    print(a ** b)
+    
