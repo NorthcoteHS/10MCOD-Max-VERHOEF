25ae6e955d947b9876963ac93d908b4f47573901
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-27 10:54:16 +1100

    VERHOEF, Max: 2018-02-27 10:54:16

---
+++ user/PigLatin2.py
@@ -0,0 +1,7 @@
+a = input("Enter a sentence to translate to pig latin ")
+vowels = ("a","e","i","o","u","A","E","I","O","U")
+a = a.split()
+b = a.find("a","e","i","o","u","A","E","I","O","U")
+print(b)
+
+
