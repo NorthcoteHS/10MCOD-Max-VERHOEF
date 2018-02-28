54ceec41f39d8895298ee46f302e98afc8dd3d74
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-28 14:53:12 +1100

    VERHOEF, Max: 2018-02-28 14:53:12

--- user/pigLatin.py
+++ user/pigLatin.py
@@ -1,7 +1,19 @@
-a = str(input("Enter a word to translate to pig latin "))
+a = str(input("Enter a sentence to translate to pig latin ")).split()
 vowels = ("a","e","i","o","u","A","E","I","O","U")
-for i in range(len(a)):
-   if a[i] in vowels:
-       print(a[i])
-a = a[i:] + a[:i]
-print(a + "ay")
+x = 0
+b = 9999999
+d = 0
+e = a[d]
+while d < len(a):
+    while x < len(vowels):
+        c = (e.find(vowels[x]))
+        x = x + 1
+        if c > -1:
+            if b > c:
+                b = c
+    print(e[b:] + e[0:b] + "ay")
+    d = d + 1
+    if d < len(a):
+        e = a[d]
+    x = 0
+    b = 9999999
---
+++ user/pigLatinOld.py
@@ -0,0 +1,7 @@
+a = str(input("Enter a word to translate to pig latin "))
+vowels = ("a","e","i","o","u","A","E","I","O","U")
+for i in range(len(a)):
+   if a[i] in vowels:
+       print(a[i])
+a = a[i:] + a[:i]
+print(a + "ay")
